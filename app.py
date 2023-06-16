e0e356d50189688ee0deec99ed25b125ac21723d
Sksop47 <shubhaman47@gmail.com>
2023-06-16 15:34:24 +0530

    removed docker just for testing if i can deploy flask directly

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ def home():
 
 
 # curl -X POST -F "file=@image.jpg" http://localhost:5000/predict > output.png
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
